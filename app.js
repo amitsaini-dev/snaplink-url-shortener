5383f4ec55570b6ca8455244fb2a4a038293de7c
Amit Saini <amitsaini.ce@gmail.com>
2026-05-29 15:45:00 +0530

    Add URL validation, duplicate checking, and redirect logic

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require("path");
 const methodOverride = require("method-override");
 const engine = require("ejs-mate"); //for layout
 const mongoose = require('mongoose');
+
+const { nanoid } = require("nanoid");
+const validator = require("validator");
+const Url = require("./models/url.js");
 const port = 8080;
 
 app.set("view engine", "ejs");
@@ -41,3 +45,28 @@ app.get("/urls/new", (req, res) => {
     res.render("urls/index.ejs");
 });
 
+app.post("/urls", async (req, res) => {
+    let { originalUrl } = req.body.url;
+    if (!validator.isURL(originalUrl)) {
+        return res.send("Invalid URL");
+    }
+    let originalUrlExist = await Url.findOne({ originalUrl });
+    if (originalUrlExist) {
+        return res.send("short url already exist");
+    }
+    const newUrl = new Url(req.body.url);
+    newUrl.shortCode = nanoid(7);
+    await newUrl.save();
+    res.send(newUrl.shortCode);
+});
+
+app.get("/:shortCode", async (req, res) => {
+    let { shortCode } = req.params;
+    const foundUrl = await Url.findOne({ shortCode });
+    foundUrl.clicks += 1;
+    await foundUrl.save();
+    if (!foundUrl) {
+        return res.send("Short URL not found");
+    }
+    res.redirect(foundUrl.originalUrl);
+});
